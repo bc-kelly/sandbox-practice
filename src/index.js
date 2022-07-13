3e7c9057332775549e8e5ebfc27c43b6ca3d2d95
bridget <97080052+bc-kelly@users.noreply.github.com>
2022-07-13 17:38:44 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Exercise
+// Reverse a string using a Stack
+
+function reverse(str) {
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    stack.push(str[i]);
+  }
+
+  let reverseStr = "";
+  while (stack.length > 0) {
+    reverseStr += stack.pop();
+  }
+  return reverseStr;
+}
+
+console.log(reverse("stack"));
